3bb2ff98c72173dfb6ae21034b92d592bc3e1cef
Elton Moura Barbosa <elton.desenvolvimento@gmail.com>
2025-12-07 15:33:52 -0500

    ajustes nos projetos

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Lógica do Menu Hamburguer
     const menuToggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
 
@@ -8,8 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
             menu.classList.toggle("menu-aberto");
             menuToggle.classList.toggle("menu-aberto");
         });
-        
-        // Fechar menu ao clicar em um link
+
         menu.querySelectorAll('.menu-link').forEach(link => {
             link.addEventListener('click', () => {
                 menu.classList.remove("menu-aberto");
@@ -24,36 +22,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-
 function enviarMensagem(event) {
     event.preventDefault();
 
-    const nomeInput = document.getElementById("nome");
-    const emailInput = document.getElementById("email");
-    const mensagemInput = document.getElementById("mensagem");
-
-    const nome = nomeInput.value.trim();
-    const email = emailInput.value.trim(); // Captura do email
-    const mensagem = mensagemInput.value.trim();
+    const nome = document.getElementById("nome").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
     
-    // Telefone com código do país e DDD (Mantenha o seu número correto!)
-    const telefone = "5568999365536"; 
+    const telefone = "5568999365536";
 
-    // Validação de campos
     if (!nome || !mensagem) {
-        alert("Por favor, preencha seu nome, email e a mensagem antes de enviar.");
+        alert("Por favor, preencha seu nome e a mensagem antes de enviar.");
         return;
     }
 
-    // Mensagem formatada para o WhatsApp
     const texto = `Olá Elton! Me chamo ${nome}. ${mensagem}`;
     const msgFormatada = encodeURIComponent(texto);
 
-    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+    window.open(`https://wa.me/${telefone}?text=${msgFormatada}`, "_blank");
 
-    // Abre o WhatsApp
-    window.open(url, "_blank");
-    
-    // Limpa o formulário após o envio
     document.getElementById("formulario").reset();
 }
